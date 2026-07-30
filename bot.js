f497ff849c5efd10e404368a3325ecd7e7e43ce9
Carlos Ignacio Olave Ruz <127158176+cxi0r@users.noreply.github.com>
2026-07-29 23:58:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ const GEAR_ITEMS = [
     "Bloodmoon Slap", "Radioactive Slap", "Rainbow Slap",
     "Rainbow Hammer", "Bloodmoon Hammer", "Radioactive Airstrike",
     "Yin Yang Lamp", "Demon's Head", "Lava Slap", "Lava Blaster",
-    "Alien Slap", "Blackhole Bomb", "Candy Sentry"
+    "Alien Slap", "Blackhole Bomb", "Candy Sentry","Phantom Slap"
 ];
 
 // ============================================================
